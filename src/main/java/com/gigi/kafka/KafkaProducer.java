0f1c1ae095c2063c84a503b53b2e85b4173909d9
xugan <xugan@kedaya.local>
2024-12-13 15:43:30 +0800

    addCallback

--- src/main/java/com/gigi/kafka/KafkaProducer.java
+++ src/main/java/com/gigi/kafka/KafkaProducer.java
@@ -1,8 +1,12 @@
 package com.gigi.kafka;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.SendResult;
 import org.springframework.stereotype.Service;
+import org.springframework.util.concurrent.ListenableFutureCallback;
 
+@Slf4j
 @Service
 public class KafkaProducer {
     private final KafkaTemplate<String, String> kafkaTemplate;
@@ -12,6 +16,18 @@ public class KafkaProducer {
     }
 
     public void sendMessage(String topic, String message) {
-        kafkaTemplate.send(topic, message);
+        Long nTime = System.currentTimeMillis();
+        kafkaTemplate.send(topic, message).addCallback(new ListenableFutureCallback<SendResult<String, String>>() {
+            @Override
+            public void onFailure(Throwable throwable) {
+                log.info("kafka消息->发送失败{topic:" + topic + ", msg:" + message + "}：" + throwable.getMessage() +
+                        ", 耗时(ms):" + (System.currentTimeMillis() - nTime), throwable);
+            }
+
+            @Override
+            public void onSuccess(SendResult<String, String> stringStringSendResult) {
+
+            }
+        });
     }
 }
